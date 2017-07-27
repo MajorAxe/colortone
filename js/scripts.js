487ead168c237877e02069219a369d68fb67171e
MajorAxe <13rgb13@gmail.com>
2017-07-27 19:09:23 +0500

    initial

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    items: 1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    animateOut: 'fadeOut'
+})
+});
+
